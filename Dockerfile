51e5c67497e0d6073c1113605d81feb5a5482ad9
necrophcodr <tcg.thegamer@gmail.com>
2014-10-06 21:28:11 +0200

    Add clone script for Docker to use. Make use of new cloned directories in Docker.

--- Dockerfile
+++ Dockerfile
@@ -8,3 +8,6 @@ rm -rf /etc/nginx/ && mkdir -p /etc/nginx && \
 mkdir -p /var/www && \
 useradd -d /var/www -s /bin/false www-data
 ADD . /
+RUN /root/scripts/clone.sh
+RUN python /var/www/html/new.hashbang.sh/setup.py install
+
---
+++ root/scripts/clone.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git clone https://github.com/hashbang/hashbang.sh /var/www/html/hashbang.sh
+git clone https://github.com/hashbang/chat /var/www/html/chat.hashbang.sh
+git clone https://github.com/hashbang/provisor /var/www/html/new.hashbang.sh
